256f0d7dd59bfd6c21f7c92606c73373dbc93133
Thiago Borges Mansano <thiagobmansano@gmail.com>
2024-05-14 21:16:50 -0300

    fix: corrigir erro de build

--- src/app/api/email/route.ts
+++ src/app/api/email/route.ts
@@ -1,15 +1,15 @@
-// import { NextRequest, NextResponse } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server';
 
-// import { ISendEmailBody } from '@/interfaces/sendEmailBody.interface';
-// import { sendMail } from '@/service/sendmail';
+import { ISendEmailBody } from '@/interfaces/sendEmailBody.interface';
+import { sendMail } from '@/service/sendmail';
 
-// export async function POST(req: NextRequest) {
-//   try {
-//     const sendMailBody: ISendEmailBody = await req.json();
-//     await sendMail(sendMailBody);
+export async function POST(req: NextRequest) {
+  try {
+    const sendMailBody: ISendEmailBody = await req.json();
+    await sendMail(sendMailBody);
 
-//     return NextResponse.json({ ...sendMail }, { status: 201 });
-//   } catch (e) {
-//     throw e;
-//   }
-// }
+    return NextResponse.json({ ...sendMail }, { status: 201 });
+  } catch (e) {
+    throw e;
+  }
+}
--- src/service/sendmail.tsx
+++ src/service/sendmail.tsx
@@ -1,32 +1,32 @@
-// import { render } from '@react-email/render';
-// import { createTransport } from 'nodemailer';
-// import SMTPTransport from 'nodemailer/lib/smtp-transport';
+import { render } from '@react-email/render';
+import { createTransport } from 'nodemailer';
+import SMTPTransport from 'nodemailer/lib/smtp-transport';
 
-// import { ISendEmailBody } from '@/interfaces/sendEmailBody.interface';
+import { ISendEmailBody } from '@/interfaces/sendEmailBody.interface';
 
-// import Contact from '../../emails/Contact';
+import Contact from '../../emails/Contact';
 
-// const transporterConfig: SMTPTransport.Options = {
-//   host: 'smtp.mailersend.net',
-//   port: 587,
-//   secure: false,
-//   auth: {
-//     user: 'MS_yJAlzl@trial-3vz9dle7m0n4kj50.mlsender.net',
-//     pass: 'e1ymD1dXQ0erSrmp',
-//   },
-// };
+const transporterConfig: SMTPTransport.Options = {
+  host: 'smtp.mailersend.net',
+  port: 587,
+  secure: false,
+  auth: {
+    user: 'MS_yJAlzl@trial-3vz9dle7m0n4kj50.mlsender.net',
+    pass: 'e1ymD1dXQ0erSrmp',
+  },
+};
 
-// export async function sendMail({ email, name, subject }: ISendEmailBody) {
-//   const transporter = createTransport(transporterConfig);
-//   const htmlContactTemplate = render(
-//     <Contact email={email} name={name} subject={subject} />
-//   );
-//   const info = await transporter.sendMail({
-//     from: '"thiag-o" <MS_yJAlzl@trial-3vz9dle7m0n4kj50.mlsender.net>',
-//     to: email,
-//     subject: 'Contato',
-//     html: htmlContactTemplate,
-//   });
+export async function sendMail({ email, name, subject }: ISendEmailBody) {
+  const transporter = createTransport(transporterConfig);
+  const htmlContactTemplate = render(
+    <Contact email={email} name={name} subject={subject} />
+  );
+  const info = await transporter.sendMail({
+    from: '"thiag-o" <MS_yJAlzl@trial-3vz9dle7m0n4kj50.mlsender.net>',
+    to: email,
+    subject: 'Contato',
+    html: htmlContactTemplate,
+  });
 
-//   console.log('Message sent: %s', info.messageId);
-// }
+  console.log('Message sent: %s', info.messageId);
+}
